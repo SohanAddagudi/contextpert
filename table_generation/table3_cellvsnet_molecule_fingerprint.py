90ac3e4d48ea82768e01fd2369698c0a6515a1c5
Sohan Addagudi <115036581+SohanAddagudi@users.noreply.github.com>
2026-03-30 23:53:21 -0400

    fingerprint adjust

--- table_generation/table3_cellvsnet_molecule_fingerprint.py
+++ table_generation/table3_cellvsnet_molecule_fingerprint.py
@@ -282,7 +282,7 @@ contextualized_model = ContextualizedCorrelation(
     context_dim=C_train.shape[1],
     x_dim=X_train.shape[1],
     encoder_type='mlp',
-    num_archetypes=30,
+    num_archetypes=50,
 )
 
 train_indices, val_indices = train_test_split(
@@ -319,7 +319,7 @@ early_stop_callback = EarlyStopping(
 )
 
 trainer = Trainer(
-    max_epochs=25,
+    max_epochs=5,
     accelerator='auto',
     devices='auto',
     callbacks=[checkpoint_callback, early_stop_callback],
